c4117375c20a8eba0f1d913a4fffae5107243ecd
Brandsma <a.brandsma@protonmail.com>
2022-12-02 11:19:30 +0100

    preprocess

---
+++ data_loader.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def load_dataset(filename="./data/train.csv"):
+    return pd.read_csv(filename)
---
+++ preprocess.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from data_loader import load_dataset
+
+def to_one_hot_encoding(df, columns):
+    for column in columns:
+        # get the dummies and store it in a variable
+        dummies = pd.get_dummies(df[column])
+
+        # Concatenate the dummies to original dataframe
+        df = pd.concat([df, dummies], axis='columns')
+
+        # drop the values
+        df = df.drop(column, axis='columns')
+    return df
+
+def discretize_categories(df, categorical_to_code_columns):
+    df[categorical_to_code_columns] = df[categorical_to_code_columns].astype('category')
+    df[categorical_to_code_columns] = df[categorical_to_code_columns].apply(lambda x: x.cat.codes)
+    return df
+
+def main():
+    df = load_dataset()
+
+    categorical_to_code_columns = ['city', 'isRoomActive']
+    df = discretize_categories(df, categorical_to_code_columns)
+
+    one_hot_encoded_columns = []
+    df = to_one_hot_encoding(df, one_hot_encoded_columns)
+
+    # print the dataframe
+    print(df.head())
+
+if __name__ == '__main__':
+    main()
