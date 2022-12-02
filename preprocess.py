7ed5e157628970e33aeb2839c057d243653f68a9
Brandsma <a.brandsma@protonmail.com>
2022-12-02 11:30:41 +0100

    preprocess

--- preprocess.py
+++ preprocess.py
@@ -20,15 +20,17 @@ def discretize_categories(df, categorical_to_code_columns):
 
 def main():
     df = load_dataset()
+    print(df.columns)
 
     categorical_to_code_columns = ['city', 'isRoomActive']
     df = discretize_categories(df, categorical_to_code_columns)
 
-    one_hot_encoded_columns = []
+    one_hot_encoded_columns = ['matchLanguages']
     df = to_one_hot_encoding(df, one_hot_encoded_columns)
 
     # print the dataframe
     print(df.head())
+    df.to_csv("./output.csv")
 
 if __name__ == '__main__':
     main()
