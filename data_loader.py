61b19ee5ab05dc9f20168454c190fcb5f8fee8e7
Brandsma <a.brandsma@protonmail.com>
2022-12-03 22:44:48 +0100

    working on cnn encoder

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def load_dataset(filename="../data/train.csv"):
+def load_dataset(filename="./data/train.csv"):
     df = pd.read_csv(filename)
     df = df.drop('id', axis=1)
     return df
