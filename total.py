9629f10766bda3473b9f4a26b54b9e212631be59
IvoSte <36919251+IvoSte@users.noreply.github.com>
2022-12-07 15:16:11 +0100

    tried to overlap prediction scores

--- total.py
+++ total.py
@@ -13,11 +13,20 @@ from xgboost import XGBRegressor
 
 
 def invTransform(scaler, data, colName, colNames):
-    dummy = pd.DataFrame(np.zeros((len(data), len(colNames))),
-                         columns=colNames)
+    dummy = pd.DataFrame(np.zeros((len(data), len(colNames))), columns=colNames)
     dummy[colName] = data
     dummy = pd.DataFrame(scaler.inverse_transform(dummy), columns=colNames)
     return dummy[colName].values
+
+
+def postprocessing(df):
+    nlp_df = load_dataset(filename="./data/test_with_nlp_prediction.csv")
+    nlp_df = nlp_df[nlp_df.rentFromNLP > 100]
+    nlp_df = nlp_df[nlp_df.rentFromNLP < 6000]
+    print(nlp_df.head())
+    for i in range(len(nlp_df)):
+        df.loc[df["id"] == nlp_df["id"][i], "rent"] = nlp_df["rentFromNLP"][i]
+    return df
 
 
 def main():
@@ -26,24 +35,24 @@ def main():
     df = load_dataset(filename=data_filename)
 
     for col in df:
-        if df[col].dtype == 'object':
-            df[col] = df[col].astype('category')
-        if df[col].dtype == 'bool':
-            df[col] = df[col].astype('float64')
-        if df[col].dtype == 'int64':
-            df[col] = df[col].astype('float64')
+        if df[col].dtype == "object":
+            df[col] = df[col].astype("category")
+        if df[col].dtype == "bool":
+            df[col] = df[col].astype("float64")
+        if df[col].dtype == "int64":
+            df[col] = df[col].astype("float64")
     # df = df.select_dtypes(exclude=['object'])  #.drop(columns="rentFromNLP")
 
     test_filename = "./data/preprocessed_data_test.csv"
     test_df = load_dataset(filename=test_filename)
 
     for col in test_df:
-        if test_df[col].dtype == 'object':
-            test_df[col] = test_df[col].astype('category')
-        if test_df[col].dtype == 'bool':
-            test_df[col] = test_df[col].astype('float64')
-        if test_df[col].dtype == 'int64':
-            test_df[col] = test_df[col].astype('float64')
+        if test_df[col].dtype == "object":
+            test_df[col] = test_df[col].astype("category")
+        if test_df[col].dtype == "bool":
+            test_df[col] = test_df[col].astype("float64")
+        if test_df[col].dtype == "int64":
+            test_df[col] = test_df[col].astype("float64")
     # test_df = test_df.select_dtypes(exclude=['object'])  #.drop(columns="rentFromNLP")
 
     test_df, _, _ = normalize_dataframe(test_df)
@@ -54,14 +63,13 @@ def main():
     orig_test = test_df
     test_df = test_df.sample(frac=1, random_state=1001)
 
-    if 'Unnamed: 0' in test_df:
-        test_df = test_df.drop('Unnamed: 0', axis = 1)
+    if "Unnamed: 0" in test_df:
+        test_df = test_df.drop("Unnamed: 0", axis=1)
 
-    test_df = test_df[df.loc[:, df.columns != 'rent'].columns]
-
+    test_df = test_df[df.loc[:, df.columns != "rent"].columns]
 
     best_params = {
-        "n_estimators": [300],  #300
+        "n_estimators": [300],  # 300
         "max_depth": [18],
         "learning_rate": [0.05],
         "subsample": [1],
@@ -71,24 +79,26 @@ def main():
     }
     param_grid = best_params
 
-    model = XGBRegressor(tree_method='hist',
-                         objective='reg:squarederror',
-                         enable_categorical=True)
+    model = XGBRegressor(
+        tree_method="hist", objective="reg:squarederror", enable_categorical=True
+    )
 
-    gs = GridSearchCV(model,
-                      param_grid,
-                      cv=5,
-                      n_jobs=-1,
-                      scoring='neg_mean_absolute_error',
-                      verbose=3,
-                      refit=True)
-    gs.fit(df.loc[:, df.columns != 'rent'], df['rent'])
+    gs = GridSearchCV(
+        model,
+        param_grid,
+        cv=5,
+        n_jobs=-1,
+        scoring="neg_mean_absolute_error",
+        verbose=3,
+        refit=True,
+    )
+    gs.fit(df.loc[:, df.columns != "rent"], df["rent"])
     # model.fit(df.loc[:, df.columns != 'rent'], df['rent'])
 
-    gs.predict(df.loc[:, df.columns != 'rent'])
+    gs.predict(df.loc[:, df.columns != "rent"])
     sweep_params = {
-        param: [setting[param] for setting in gs.cv_results_['params']]
-        for param in gs.cv_results_['params'][0]
+        param: [setting[param] for setting in gs.cv_results_["params"]]
+        for param in gs.cv_results_["params"][0]
     }
 
     # gs_results = [gs.cv_results_[f'split{x}_test_score'][0] for x in range(5)]
@@ -103,19 +113,19 @@ def main():
     # print(scaler.inverse_transform(gs.cv_results_['mean_test_score']))
     # exit()
 
-    results = pd.DataFrame({
-        **sweep_params, "mean_test_score":
-        gs.cv_results_['mean_test_score'],
-        "rank_test_score":
-        gs.cv_results_['rank_test_score']
-    })
-    print(results.sort_values('rank_test_score'))
+    results = pd.DataFrame(
+        {
+            **sweep_params,
+            "mean_test_score": gs.cv_results_["mean_test_score"],
+            "rank_test_score": gs.cv_results_["rank_test_score"],
+        }
+    )
+    print(results.sort_values("rank_test_score"))
     results.to_csv(os.path.join("out", "parameter_sweep.csv"))
 
     if not using_test_set:
         return
     print("Testing...")
-
 
     print("Generating submission...")
     predictions = gs.predict(test_df)
@@ -125,7 +135,6 @@ def main():
     pred_df = pred_df.reindex(orig_test.index)
 
     # nlp_df = load_dataset(filename="./data/train_with_nlp_prediction.csv")
-
 
     # TODO: Overlay the NLP dataset
     # better_nlp_df = filter_predictions(nlp_df)
@@ -146,7 +155,15 @@ def main():
     pred_df.to_csv("./new_submission.csv")
 
 
+def test():
+    df = load_dataset("./new_submission.csv")
+    postprocessing(df)
+    df.to_csv("./new_submission_2.csv")
+
+
 # to beat, 87.51 (global 84)
 # Next to beat, 85.97
-if __name__ == '__main__':
+if __name__ == "__main__":
+    test()
+    exit()
     main()
